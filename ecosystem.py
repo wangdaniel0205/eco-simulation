cd94bdc21f4ba116624a405b687a4254fb5c1599
Cappuco <44463567+Cappuco@users.noreply.github.com>
2019-07-15 23:12:23 +0900

    daniel 1

--- ecosystem.py
+++ ecosystem.py
@@ -1,19 +1,26 @@
-
+import constants as cs
 import tkinter as tk
-#from numpy import array, empty
+from numpy import array, empty
 
 class MainAppliaction(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+        root.title("너가 정해라 이거 ㅋㅋㅋ")
+        root.geometry("500x500")
+        root.resizable(0, 0)
 
-        
+        self.world = tk.Canvas(root, width=1000, height=1000, highlightthickness=0, background="{}".format("#%02x%02x%02x" % (249,248,249)))
+        self.world.pack()
 
-
-
-#class Map(object):
-#    def __init__(self):
-#        self.world = tk.Canvas(root, width=1000, height=1000, background="{}".format("#%02x%02x%02x" % (249,248,249)))
-#        self.world.pack()
+        self.grids = empty([100,100])
+        for col in range (0,100):
+            for row in range (0,100):
+                if cs.mapValues[col,row] == 0:
+                    self.grids[col,row] = self.world.create_rectangle(row*cs.gridSize, col*cs.gridSize,(row+1)*cs.gridSize, (col+1)*cs.gridSize, fill="{}".format(cs.mapColors[0]), outline="{}".format(cs.mapColors[0]))
+                elif cs.mapValues[col,row] == 1:
+                    self.grids[col,row] = self.world.create_rectangle(row*cs.gridSize, col*cs.gridSize,(row+1)*cs.gridSize, (col+1)*cs.gridSize, fill="{}".format(cs.mapColors[1]), outline="{}".format(cs.mapColors[1]))
+                elif cs.mapValues[col,row] == 2:
+                    self.grids[col,row] = self.world.create_rectangle(row*cs.gridSize, col*cs.gridSize,(row+1)*cs.gridSize, (col+1)*cs.gridSize, fill="{}".format(cs.mapColors[2]), outline="{}".format(cs.mapColors[2]))
 
 
 
